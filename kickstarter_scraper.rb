f8565c067bc58dc50f967fb3439427b8325f3bee
Sonja Parsell <hello@sparsell.com>
2020-05-19 16:35:45 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,11 +3,11 @@ require "pry"
 
 def create_project_hash
   projects = {}
-  
+
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
-  
+
 
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
